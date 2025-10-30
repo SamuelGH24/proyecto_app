a483526c3932c36ec912d24254d94d6b6b87a7d7
SamulGH24 <sgerena56@uan.edu.co>
2025-10-30 15:16:47 -0500

    Renombrado credenciales.env a .env y actualización del servidor

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '2424',
   database: process.env.DB_NAME || 'uan_db',
   multipleStatements: false
 });
